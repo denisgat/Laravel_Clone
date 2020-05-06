f45fd945bb8c9328f3fcbd9fc980db734baf1af1
denisgat <denisgatotho@gmail.com>
2020-05-06 13:50:16 -0400

    trying to fix

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\User;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users', function(){
+    return User::all();
+});
+
+Route::post('/register', 'AuthController@register');
+Route::post('/login', 'AuthController@login');
